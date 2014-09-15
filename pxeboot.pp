b507622a5d2826cca8cfc179d1634b5c2cfb3589
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2014-09-15 19:40:39 +0400

    /etc/alternatives nfs export

--- pxeboot.pp
+++ pxeboot.pp
@@ -72,6 +72,7 @@ file { "/etc/exports":
 	content => "
 /nfsroot	192.168.10.0/24(rw,no_root_squash,async,insecure,fsid=0,nohide)
 /nfsroot/bin	192.168.10.0/24(rw,no_root_squash,async,insecure,nohide)
+/nfsroot/etc/alternatives	192.168.10.0/24(rw,no_root_squash,async,insecure,nohide)
 /nfsroot/home	192.168.10.0/24(rw,no_root_squash,async,insecure,nohide)
 /nfsroot/lib	192.168.10.0/24(rw,no_root_squash,async,insecure,nohide)
 /nfsroot/root	192.168.10.0/24(rw,no_root_squash,async,insecure,nohide)
